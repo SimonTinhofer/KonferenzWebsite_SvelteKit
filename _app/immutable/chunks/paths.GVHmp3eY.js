e1940bc85a5ac39bef993c2e750a4213d0335cdd
SimonTinhofer <tinhofer.simon@gmail.com>
2024-01-13 23:17:39 +0100

    Updates

--- _app/immutable/chunks/paths.GVHmp3eY.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1dtiy1i?.base??"",t=globalThis.__sveltekit_1dtiy1i?.assets??s;export{t as a,s as b};
---
+++ _app/immutable/chunks/paths.hk-0Aema.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_19upqb7?.base??"",a=globalThis.__sveltekit_19upqb7?.assets??s;export{a,s as b};
