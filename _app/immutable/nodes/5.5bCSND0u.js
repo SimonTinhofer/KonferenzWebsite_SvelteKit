f54a88a3f1edaa4df1452195f0f9aa3f29af3d85
SimonTinhofer <tinhofer.simon@gmail.com>
2024-01-13 22:36:22 +0100

    Updates

---
+++ _app/immutable/nodes/5.5bCSND0u.js
@@ -0,0 +1,10 @@
+import{s as i,n as s}from"../chunks/scheduler.afOcmUe7.js";import{S as l,i as h,g as c,h as d,x as o,k as b,a as p,f as m}from"../chunks/index.fo2pth-m.js";function f(n){let e,r=`<h1 class="svelte-55pb4d">Information nach § 5 Abs 1 ECG</h1> <p class="svelte-55pb4d">Dr. Andreas Tinhofer, LL.M.<br/>
+        Rechtsanwalt<br/>
+        Heumarkt 7/7/93<br/>
+        1030 Wien<br/>
+        T  +43 664 810 66 59<br/>
+        E  tinhofer@tinhofer.legal<br/>
+        W www.tinhofer.legal<br/>
+        UID ATU 57327878<br/>
+        IBAN AT112011128643633202</p> <p class="svelte-55pb4d">Wir sind Rechtsanwälte nach österreichischem Recht und unterliegen dem einschlägigen Standesrecht (RAO, RL-BA), abrufbar unter <a href="http://www.oerak.at" class="svelte-55pb4d">http://www.oerak.at</a>, sowie der Aufsicht der Rechtsanwaltskammer Wien.</p> <h2 class="svelte-55pb4d">Disclaimer</h2> <p class="svelte-55pb4d">Für den Inhalt dieser Website ist Herr RA Dr. Andreas Tinhofer, LL.M., alleinverantwortlich. Sämtliche Inhalte dienen ausschließlich der Information und stellen keine Rechtsberatung dar. Für die Richtigkeit dieser Informationen sowie für die Inhalte der verlinkten Websites wird keine Haftung übernommen.</p> <h2 class="svelte-55pb4d">Urheberrechtsschutz</h2>
+    Die Texte auf der Website sind urheberrechtlich geschützt.`;return{c(){e=c("div"),e.innerHTML=r,this.h()},l(t){e=d(t,"DIV",{class:!0,"data-svelte-h":!0}),o(e)!=="svelte-aw2p35"&&(e.innerHTML=r),this.h()},h(){b(e,"class","container svelte-55pb4d")},m(t,a){p(t,e,a)},p:s,i:s,o:s,d(t){t&&m(e)}}}class v extends l{constructor(e){super(),h(this,e,null,f,i,{})}}export{v as component};
